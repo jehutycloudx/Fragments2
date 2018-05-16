ca57a3c7db8ce53af680f14893beefa22bf6b34c
jcloud <splendidseacollection@gmail.com>
2018-05-16 14:42:49 -0400

    New commit 5/16

---
+++ app/src/main/java/com/jehutyanubis/fragments/MainActivity.java
@@ -0,0 +1,55 @@
+package com.jehutyanubis.fragments;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager; /*created in activity main XML all the way at the bottom*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Started.");
+
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        /* create view pager object above ^^ as that is what we will be referencing*/
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+        /* reference the container as this is what we will be swapping the fragments out of*/
+        //setup the pager below
+
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment1(), "Fragment1");
+        adapter.addFragment(new Fragment2(), "Fragment2");
+        adapter.addFragment(new Fragment3(), "Fragment3");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
+
+    /*Testing git */
+
+    public Button JButton;
+
+}
